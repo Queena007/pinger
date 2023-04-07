f34d4d629f30bb3fd206a9a29760f8d30639f6a6
Queena007 <124480642+Queena007@users.noreply.github.com>
2023-04-07 13:42:16 -0400

    Update pinger.py

--- pinger.py
+++ pinger.py
@@ -6,8 +6,6 @@ import time
 import select
 import binascii
 import pandas as pd
-import warnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
 
 ICMP_ECHO_REQUEST = 8
 
@@ -33,11 +31,10 @@ def checksum(string):
     answer = answer >> 8 | (answer << 8 & 0xff00)
     return answer
 
-
 def receiveOnePing(mySocket, ID, timeout, destAddr):
     timeLeft = timeout
 
-    while timeLeft > 0:
+    while 1:
         startedSelect = time.time()
         whatReady = select.select([mySocket], [], [], timeLeft)
         howLongInSelect = (time.time() - startedSelect)
@@ -47,42 +44,49 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
         timeReceived = time.time()
         recPacket, addr = mySocket.recvfrom(1024)
 
+        # Fill in start
+
         # Fetch the ICMP header from the IP packet
         icmpHeader = recPacket[20:28]
         icmpType, code, mychecksum, packetID, sequence = struct.unpack("bbHHh", icmpHeader)
-
-        if icmpType == ICMP_ECHO_REQUEST and packetID == ID:
+   
+        if type != 8 and packetID == ID:
             bytesInDouble = struct.calcsize("d")
             timeSent = struct.unpack("d", recPacket[28:28 + bytesInDouble])[0]
             return timeReceived - timeSent
 
-        timeLeft -= howLongInSelect
-    return "Request timed out."
-
+        # Fill in end
+        timeLeft = timeLeft - howLongInSelect
+        if timeLeft <= 0:
+            return "Request timed out."
 
 def sendOnePing(mySocket, destAddr, ID):
     # Header is type (8), code (8), checksum (16), id (16), sequence (16)
-    myChecksum = 0
 
+    myChecksum = 0
     # Make a dummy header with a 0 checksum
     # struct -- Interpret strings as packed binary data
     header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
     data = struct.pack("d", time.time())
-
     # Calculate the checksum on the data and the dummy header.
     myChecksum = checksum(header + data)
 
     # Get the right checksum, and put in the header
+
     if sys.platform == 'darwin':
+        # Convert 16-bit integers from host to network  byte order
         myChecksum = htons(myChecksum) & 0xffff
     else:
         myChecksum = htons(myChecksum)
+
 
     header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
     packet = header + data
 
     mySocket.sendto(packet, (destAddr, 1))  # AF_INET address must be tuple, not str
 
+    # Both LISTS and TUPLES consist of a number of objects
+    # which can be referenced by their position number within the object.
 
 def doOnePing(destAddr, timeout):
     icmp = getprotobyname("icmp")
@@ -90,18 +94,12 @@ def doOnePing(destAddr, timeout):
     # SOCK_RAW is a powerful socket type. For more details:   https://sock-raw.org/papers/sock_raw
     mySocket = socket(AF_INET, SOCK_RAW, icmp)
 
-  
-    myID = os.get
-myID = os.getpid() & 0xFFFF  # Return the current process i
+    myID = os.getpid() & 0xFFFF  # Return the current process i
     sendOnePing(mySocket, destAddr, myID)
     delay = receiveOnePing(mySocket, myID, timeout, destAddr)
     mySocket.close()
-    
-    #check whether the delay is a string or a float
-    if isinstance(delay, str):
-        return -1
-    else:
-        return delay
+    return delay
+
 def ping(host, timeout=1):
     # timeout=1 means: If one second goes by without a reply from the server,  
     # the client assumes that either the client's ping or the server's pong is lost
@@ -113,18 +111,11 @@ def ping(host, timeout=1):
    
     #Send ping requests to a server separated by approximately one second
     #Add something here to collect the delays of each ping in a list so you can calculate vars after your ping
-    ttl = 0
+    ttl=0
     for i in range(0,4): #Four pings will be sent (loop runs for i=0, 1, 2, 3)
         delay = doOnePing(dest, timeout) #what is stored into delay and statistics?
-        ttl += 1
-     
-
-        if delay == -1:
-            print("Request timed out.")
-        else:
-            response = response.append({'bytes':8,'rtt':delay,'ttl':ttl}, ignore_index=True)
-
-        #store your bytes, rtt, and ttle here in your response pandas dataframe. An example is commented out below for vars
+        ttl+=1
+        response = response.append({'bytes':8,'rtt':delay,'ttl':ttl},ignore_index=True)#store your bytes, rtt, and ttle here in your response pandas dataframe. An example is commented out below for vars
         print(delay)
         time.sleep(1)  # wait one second
    
@@ -140,16 +131,9 @@ def ping(host, timeout=1):
 
     #You should have the values of delay for each ping here structured in a pandas dataframe;
     #fill in calculation for packet_min, packet_avg, packet_max, and stdev
-    
     vars = pd.DataFrame(columns=['min', 'avg', 'max', 'stddev'])
-
-    if len(response) > 0:
-        rtt = response['rtt'].dropna()
-        if len(rtt) > 0:
-            vars = vars.append({'min': round(float(rtt.min()), 2), 'avg': round(float(rtt.mean()), 2),
-                                'max': round(float(rtt.max()), 2), 'stddev': round(float(rtt.std()), 2)}, ignore_index=True)
-
-    print(vars) #make sure your vars data you are returning resembles acceptance criteria
+    vars = vars.append({'min':str(round(response['rtt'].min(), 2)), 'avg':str(round(response['rtt'].mean(), 2)),'max':str(round(response['rtt'].max(), 2)), 'stddev':str(round(response['rtt'].std(),2))}, ignore_index=True)
+    print (vars) #make sure your vars data you are returning resembles acceptance criteria
     return vars
 
 if __name__ == '__main__':
